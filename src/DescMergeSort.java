01cec408ba7aabd154a3bc24d74d9e3a7055712f
Anukul <anukul.p@growthsourceft.com>
2021-08-30 17:12:06 +0530

    merge sort descending

--- src/DescMergeSort.java
+++ src/DescMergeSort.java
@@ -1,28 +1,27 @@
-public class MergeSort {
+public class DescMergeSort {
     public static void main(String[] args) {
         int[] intArray = {20,35,-15,7,55,1,-22};
+
         mergeSort(intArray,0, intArray.length);
 
-        for(int i =0;i< intArray.length;i++){
+        for(int i =0;i<intArray.length;i++){
             System.out.println(intArray[i]);
         }
     }
 
-    public static void mergeSort(int[] input, int start, int end ){
+    public static void mergeSort(int[] input, int start, int end){
         if(end-start<2){
             return;
         }
 
-        int mid = (start+end)/2;
-
-        mergeSort(input, start,mid);
-        mergeSort(input, mid,end);
+        int mid=(start+end)/2;
+        mergeSort(input,start,mid);
+        mergeSort(input,mid,end);
         merge(input,start,mid,end);
     }
 
-//    20,35,-15,7,55,1-22
-    public static void merge(int[] input, int start, int mid, int end) {
-        if(input[mid-1]<=input[mid]){
+    public static void merge(int[] input, int start, int mid, int end){
+        if(input[mid-1]>=input[mid]){
             return;
         }
 
@@ -31,8 +30,9 @@ public class MergeSort {
         int tempIndex = 0;
 
         int[] temp = new int[end-start];
+
         while(i<mid && j<end){
-            temp[tempIndex++]= input[i]<=input[j]?input[i++]:input[j++];
+            temp[tempIndex++]=input[i]>=input[j]?input[i++]:input[j++];
         }
 
         System.arraycopy(input,i,input,start+tempIndex,mid-i);
